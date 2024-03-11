f1b4405e948d05dd7f52e03c4e2dec4b0f80aa0c
Elobarros <eloizamariayou@gmail.com>
2024-03-11 13:42:03 -0300

    meu primeiro commit

---
+++ teste.py
@@ -0,0 +1,33 @@
+def velocidade_media(m,s):
+    print(m+s/2)
+
+    velocidade_media(20,20)
+
+def velocidade_media2(m,s):
+    vm=m+s/2
+    return vm
+
+print(velocidade_media2(15,10))
+
+
+def calcular (m/s):
+ dividir=m/2
+ return {dividir}
+print (calcular(12,3))
+
+
+def
+
+
+
+
+
+
+
+
+
+
+
+
+
+
